31911a2b730ce6f076b227c4a1248b3fda921e69
paulamachadomt <paulamachadomt@gmail.com>
2021-11-24 22:43:26 -0300

    application.properties

--- src/main/java/edu/usj/crudcontatosbackend/ContatoEntity.java
+++ src/main/java/edu/usj/crudcontatosbackend/ContatoEntity.java
@@ -1,9 +1,6 @@
 package edu.usj.crudcontatosbackend;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
